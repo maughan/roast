7b9188b3466eb2d21e654e387f82f73a411571b7
Alistair Smith <alistairsmith01@gmail.com>
2021-04-23 21:39:15 +0100

    vscodemoment

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,10 @@ interface FormValues {
 }
 
 const toBase64 = (file: File): Promise<string> => {
-  new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
+    reader.onload = () => resolve(reader.result as string);
     reader.onerror = error => reject(error);
   });
 };
