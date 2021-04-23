da2a6658c7fbe9da9e0dd88291992dbf09717e31
Alistair Smith <alistairsmith01@gmail.com>
2021-04-23 22:06:46 +0100

    ROAST TS IS NOT BEING HIT

--- pages/api/roast.ts
+++ pages/api/roast.ts
@@ -1,10 +1,11 @@
-import {NextApiRequest, NextApiResponse} from "next";
 import * as vision from "@google-cloud/vision";
 import {HttpException, withCatch} from "../../lib/http";
 
 const client = new vision.ImageAnnotatorClient();
 
-export default withCatch(async function (req: NextApiRequest, res: NextApiResponse) {
+export default withCatch(async (req, res) => {
+  console.log("BRUH LOL");
+
   if (req.method !== "POST") {
     throw new HttpException(405, "You must POST to this route.");
   }
