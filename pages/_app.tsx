0e88ec91bc03e0ce844c0d3fc5e64e01bbb3e156
Alistair Smith <alistairsmith01@gmail.com>
2021-04-23 20:10:30 +0100

    discord server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {ChakraProvider} from "@chakra-ui/react";
 import {AppProps} from "next/app";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>plsroast.me</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
