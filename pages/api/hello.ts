4192375304a4902925b1298a5d482f8282855ccb
Rhys <rhys@Rhyss-MacBook-Pro.local>
2021-04-23 21:27:51 +0100

    setup api route

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,12 +1,14 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import * as vision from "@google-cloud/vision";
 
-export default async function processImage(req: NextApiRequest, res: NextApiResponse) {
-  const client = new vision.ImageAnnotatorClient();
-  const [results] = await client.labelDetection("");
-  const labels = results.labelAnnotations;
-  console.log("labels:");
-  if (!labels) return;
-  labels.forEach(label => console.log(label.description));
-  res.status(200);
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    // const client = new vision.ImageAnnotatorClient();
+    // const [results] = await client.labelDetection("");
+    // const labels = results.labelAnnotations;
+    // console.log("labels:");
+    // if (!labels) return;
+    // labels.forEach(label => console.log(label.description));
+    return res.status(200).json({hello: "world"});
+  }
 }
