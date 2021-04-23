f4983b7d0ac21ba3342bbfe251916d83cb214c11
Alistair Smith <alistairsmith01@gmail.com>
2021-04-23 20:06:12 +0100

    add the

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,20 @@
 import React from "react";
-import {Heading} from "@chakra-ui/layout";
+import {Box, Heading, Link, Text} from "@chakra-ui/layout";
 
 export default function Home() {
-  return <Heading size="4xl">Roast Me balls</Heading>;
+  return (
+    <Box textAlign="center" paddingTop="30px">
+      <Heading size="4xl">Roast Me balls</Heading>
+      <Text>
+        Another stupid project by{" "}
+        <Link color="blue.500" href="https://twitter.com/aabbccsmith">
+          @aabbccsmith
+        </Link>{" "}
+        and{" "}
+        <Link color="blue.500" href="https://twitter.com/rsjm_io">
+          @rsjm_io
+        </Link>
+      </Text>
+    </Box>
+  );
 }
