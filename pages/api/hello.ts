743d14b0497e5c269b328feb7592d4de014baa61
Rhys <rhys@Rhyss-MacBook-Pro.local>
2021-04-23 21:32:16 +0100

    pog

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,12 +3,13 @@ import * as vision from "@google-cloud/vision";
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    // const client = new vision.ImageAnnotatorClient();
-    // const [results] = await client.labelDetection("");
-    // const labels = results.labelAnnotations;
-    // console.log("labels:");
-    // if (!labels) return;
-    // labels.forEach(label => console.log(label.description));
+    const {buffer} = req.body;
+    const client = new vision.v1.ImageAnnotatorClient();
+    const [results] = await client.labelDetection(buffer);
+    const labels = results.labelAnnotations;
+    console.log("labels:");
+    if (!labels) return;
+    labels.forEach(label => console.log(label.description));
     return res.status(200).json({hello: "world"});
   }
 }
