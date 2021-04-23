77ea0983125933e1f76ca7661ddbeb436c81243a
Rhys <rhys@Rhyss-MacBook-Pro.local>
2021-04-23 22:01:36 +0100

    merge

--- pages/api/roast.ts
+++ pages/api/roast.ts
@@ -2,20 +2,22 @@ import {NextApiRequest, NextApiResponse} from "next";
 import * as vision from "@google-cloud/vision";
 import {HttpException, withCatch} from "../../lib/http";
 
-const client = new vision.ImageAnnotatorClient();
+const client = new vision.default.ImageAnnotatorClient();
 
 export default withCatch(async function (req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     throw new HttpException(405, "You must POST to this route.");
   }
-  const buffer = req.body;
-  const [results] = await client.faceDetection(buffer);
-  console.log("labels:");
-  console.log(results);
 
-  if (!results.labelAnnotations) {
-    throw new HttpException(500, "Could not find any info for this image");
+  if (req.body) {
+    const [results] = await client.faceDetection(req.body);
+    console.log("labels:");
+
+    if (!results.labelAnnotations) {
+      throw new HttpException(500, "Could not find any info for this image");
+    }
+
+    res.json(results.labelAnnotations);
   }
-
-  res.json(results.labelAnnotations);
+  res.send(200);
 });
