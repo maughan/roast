cba10b2ed941dc60f76e17ef663bc75c22f70ef2
Alistair Smith <alistairsmith01@gmail.com>
2021-04-23 21:31:59 +0100

    feat: http exception setup

--- pages/api/roast.ts
+++ pages/api/roast.ts
@@ -1,14 +1,18 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import * as vision from "@google-cloud/vision";
+import {HttpException, withCatch} from "../../lib/http";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    // const client = new vision.ImageAnnotatorClient();
-    // const [results] = await client.labelDetection("");
-    // const labels = results.labelAnnotations;
-    // console.log("labels:");
-    // if (!labels) return;
-    // labels.forEach(label => console.log(label.description));
-    return res.status(200).json({hello: "world"});
+const client = new vision.ImageAnnotatorClient();
+
+export default withCatch(async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== "POST") {
+    throw new HttpException(405, "You must POST to this route.");
   }
-}
+
+  const [results] = await client.faceDetection("");
+  const labels = results.labelAnnotations;
+  console.log("labels:");
+  if (!labels) return;
+
+  res.json(labels);
+});
